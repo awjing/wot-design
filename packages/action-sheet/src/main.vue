258b279d91fb9039e8de7c419785556a7d5e4206
wangjing <wangjing90@jd.com>
2021-01-30 10:59:58 +0800

    feat(actionSheet): 控制面板监听浏览器事件，关闭面板

--- packages/action-sheet/src/main.vue
+++ packages/action-sheet/src/main.vue
@@ -102,11 +102,6 @@ export default {
       default: 200
     }
   },
-  activated () {
-    window.addEventListener('popstate', () => {
-      this.close()
-    }, false)
-  },
   computed: {
     isSingle () {
       return this.panels.length && !(this.panels[0] instanceof Array)
